ca74205e0eb4ddbeb913aa3314dfaf29ffe073ea
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-15 02:57:28 +0900

    Add files via upload

--- Script/ControllerCharacters.cs
+++ Script/ControllerCharacters.cs
@@ -10,9 +10,11 @@ public class ControllerCharacters : Monobehaivior
     public float jumpHeight = 2f;
     public float dashDistance = 5f;
 
+    public float gravity = -9.81f;
+    public Vector3 drags;
+
     private CharacterController characterController;
 
-    // 5:14
 
     private Vector3 inputDirection = Vector3.zero;
 
@@ -20,53 +22,64 @@ public class ControllerCharacters : Monobehaivior
     public LayerMask groundLayerMask;
     public float groundCheckDistance = 0.3f;
 
+    private Vector3 calcVelocity;
+
     // Start is called before the first frame update
 
     void Start()
     {
-        rigidbody = GetComponent<RigidBody>();
+        characterController = GetComponent<CharacterController>();
 
     }
 
     void Update()
     {
-        CheckGroundStatus();
-        // Process user inputs
-        inputDirection = Vector3.zero;
-        inputDirection.x = Input.GetAxis("Horizontal");
-        inputDirection.z = Input.GetAxis("Vertical");
-        if (inputDirection != Vector3.zero)
+        isGrounded = characterController.isGrounded;
+
+        if (isGrounded && calcVelocity.y < 0)
+        {
+            calcVelocity = 0;
+        }
+
+        // Process move inputs
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(move * Time.deltaTime * speed);
+        if (move != Vector3.zero)
         {
             transform.forward = inputDirection;
         }
-
+                
         // Process jump input
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             // 점프에 대한 계산공식
-            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
-
-            // 가속도로 받은 힘을 입력
-            rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+            calcVelocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
         }
 
         // Process dash input
 
         if (Input.GetButtonDown("Dash"))
         {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime),
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * drags.x + 1)) / -Time.deltaTime),
              0,
-             (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
-            rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
+             (Mathf.Log(1f / (Time.deltaTime * drags.z + 1)) / -Time.deltaTime)));
+            calcVelocity += dashVelocity;
         }
 
+
+        // Progress gravity
+        calcVelocity.y += gravity * Time.deltaTime;
+
+        // Process dash ground drags
+        calcVelocity.x /= 1 + drags.x * Time.deltaTime;
+        calcVelocity.y /= 1 + drags.y * Time.deltaTime;
+        calcVelocity.z /= 1 + drags.z * Time.deltaTime;
+
+        characterController.Move(calcVelocity * Time.deltaTime);
     }
 
-    private void FixedUpdate()
-    {
-        rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);
-    }
-
+    
     #region Helper Methods
     void CheckGroundStatus()
     {
