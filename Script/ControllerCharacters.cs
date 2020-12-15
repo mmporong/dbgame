8bc4263ac16584ff3d8b7f537e59fa8909ad8ceb
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-15 10:48:41 +0900

    Add files via upload

--- Script/ControllerCharacters.cs
+++ Script/ControllerCharacters.cs
@@ -6,19 +6,13 @@ public class ControllerCharacters : Monobehaivior
 {
 
     #region Variables
-    public float speed = 5f;
-    public float jumpHeight = 2f;
-    public float dashDistance = 5f;
-
-    public float gravity = -9.81f;
-    public Vector3 drags;
 
     private CharacterController characterController;
-
-
-    private Vector3 inputDirection = Vector3.zero;
+    private NavMeshAgent agent;
+    private Camera camera;
 
     private bool isGrounded = false;
+
     public LayerMask groundLayerMask;
     public float groundCheckDistance = 0.3f;
 
@@ -29,76 +23,47 @@ public class ControllerCharacters : Monobehaivior
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        agent = GetComponent<NavmeshAgent>();
+        agent.updatePosition = false;
+        agent.updateRotation = true;
+
+        camera = Camera.main;
 
     }
 
     void Update()
     {
-        isGrounded = characterController.isGrounded;
-
-        if (isGrounded && calcVelocity.y < 0)
+        // Process left button input
+        if (Input.GetMouseButtonDown(0))
         {
-            calcVelocity = 0;
+            // Make ray from screen to world
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            // Check hit from ray
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, groundLayerMask))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+
+                // Move our character to what we hit
+                agent.SetDestination(hit.point);
+            }
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                characterController.Move(agent.velocity * Time.deltaTime);
+
+            }
+            else
+            {
+                characterController.Move(Vector3.zero);
+            }
         }
-
-        // Process move inputs
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * speed);
-        if (move != Vector3.zero)
-        {
-            transform.forward = inputDirection;
-        }
-                
-        // Process jump input
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            // 점프에 대한 계산공식
-            calcVelocity.y += Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
-        }
-
-        // Process dash input
-
-        if (Input.GetButtonDown("Dash"))
-        {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * drags.x + 1)) / -Time.deltaTime),
-             0,
-             (Mathf.Log(1f / (Time.deltaTime * drags.z + 1)) / -Time.deltaTime)));
-            calcVelocity += dashVelocity;
-        }
-
-
-        // Progress gravity
-        calcVelocity.y += gravity * Time.deltaTime;
-
-        // Process dash ground drags
-        calcVelocity.x /= 1 + drags.x * Time.deltaTime;
-        calcVelocity.y /= 1 + drags.y * Time.deltaTime;
-        calcVelocity.z /= 1 + drags.z * Time.deltaTime;
-
-        characterController.Move(calcVelocity * Time.deltaTime);
     }
 
-    
-    #region Helper Methods
-    void CheckGroundStatus()
+    private void LateUpdate()
     {
-        RaycastHit hitInfo;
-
-#if UNITY_EDITOR
-        Debug.DrawLine(transform.position + (Vector3.up * 0.1f),
-        transform.position + (Vector3.up * 0.1f) + (Vector3.down * grounCheckDistance));
-#endif
-        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f),
-            Vector3.down, out hitInfo, groundCheckDistance, groundLayerMask))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
+        transform.position = agent.nextPosition;
     }
-    #endregion Helper Methods
-}
+
+}   
