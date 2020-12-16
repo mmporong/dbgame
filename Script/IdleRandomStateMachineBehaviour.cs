d1a15b1d433bc0d5f2a23de9b94b9e330defa454
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-16 16:22:12 +0900

    Add files via upload

---
+++ Script/IdleRandomStateMachineBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleRandomStateMachineBehaviour : StateMachineBehaviour
+{
+    #region Variables
+    public int numberOfStates = 2;
+    public float minNormTime = 2f;
+    public float maxNormTime = 5f;
+
+    public float randomNormalTime;
+
+    readonly int hashRandomIdle = Animator.StringToHash("RandomIdle");
+
+    #endregion Variables
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Randomly decide a time at which to transition.
+        randomNormalTime = Random.Range(minNormTime, maxNormTime);
+    }
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // If tranitioning asway from this state reset the random idle parameter to -1
+        if (animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).fullPathHash == stateInfo.fullPathHash)
+        {
+            animator.SetInteger(hashRandomIdle, -1);
+        }
+
+        // If the state is beyond the randomly decide normalised time and not yet transitioning
+        if (stateInfo.normalizedTime > randomNormalTime && !animator.IsInTransition(0))
+        {
+            animator.SetInteger(hashRandomIdle, Random.Range(0, numberOfStates));
+
+        }
+
+    }
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Script/RigidBodyCharacter.cs
+++ Script/RigidBodyCharacter.cs
@@ -1,15 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidBodyCharacter : Monobehaivior {
+public class RigidBodyCharacter : MonoBehaviour
+{
 
     #region Variables
     public float speed = 5f;
     public float jumpHeight = 2f;
     public float dashDistance = 5f;
 
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
 
     private Vector3 inputDirection = Vector3.zero;
 
@@ -17,27 +18,37 @@ public class RigidBodyCharacter : Monobehaivior {
     public LayerMask groundLayerMask;
     public float groundCheckDistance = 0.3f;
 
-    // Start is called before the first frame update
-    
-    void Start() 
-    {
-        rigidbody = GetComponent<RigidBody>();
 
+    #endregion Variables
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update() 
+    // Update is called once per frame
+    void Update()
     {
         CheckGroundStatus();
-        // Process user inputs
+
         inputDirection = Vector3.zero;
         inputDirection.x = Input.GetAxis("Horizontal");
         inputDirection.z = Input.GetAxis("Vertical");
-        if (inputDirection != Vector3.zero) {
+        if (inputDirection != Vector3.zero)
+        {
             transform.forward = inputDirection;
         }
 
         // Process jump input
-        if (Input.GetButtonDown("Jump") && isGrounded) {
+
+        if (inputDirection != Vector3.zero)
+        {
+            transform.forward = inputDirection;
+        }
+
+        // Process jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             // 점프에 대한 계산공식
             Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
 
@@ -47,36 +58,38 @@ public class RigidBodyCharacter : Monobehaivior {
 
         // Process dash input
 
-        if (Input.GetButtonDown("Dash")) {
-            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / - Time.deltaTime),
-             0, 
-             (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / - Time.deltaTime)));
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime),
+             0,
+             (Mathf.Log(1f / (Time.deltaTime * rigidbody.drag + 1)) / -Time.deltaTime)));
             rigidbody.AddForce(dashVelocity, ForceMode.VelocityChange);
         }
-   
     }
-
-    private void FixedUpdate(){
+    private void FixedUpdate()
+    {
         rigidbody.MovePosition(rigidbody.position + inputDirection * speed * Time.fixedDeltaTime);
     }
 
     #region Helper Methods
-    void CheckGroundStatus() {
+    void CheckGroundStatus()
+    {
         RaycastHit hitInfo;
 
 #if UNITY_EDITOR
         Debug.DrawLine(transform.position + (Vector3.up * 0.1f),
-        transform.position + (Vector3.up * 0.1f) + (Vector3.down * grounCheckDistance));
+        transform.position + (Vector3.up * 0.1f) + (Vector3.down * groundCheckDistance));
 #endif
         if (Physics.Raycast(transform.position + (Vector3.up * 0.1f),
             Vector3.down, out hitInfo, groundCheckDistance, groundLayerMask))
         {
             isGrounded = true;
-        } else
+        }
+        else
         {
             isGrounded = false;
         }
 
     }
-#endregion Helper Methods
+    #endregion Helper Methods
 }
