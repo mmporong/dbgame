94d878f33df7c1ed0c478a127fd2658c278706be
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-23 11:35:23 +0900

    Add files via upload

---
+++ Script/EnemyController_New.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController_New : MonoBehaviour
+{
+    #region Variables
+    protected StateMachine<EnemyController_New> stateMachine;
+
+    #endregion Variables
+
+    #region Unity Methods
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    #endregion Unity Methods
+}
