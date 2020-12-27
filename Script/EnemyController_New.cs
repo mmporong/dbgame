ae11e45ccf2a928a1adcb72fec8c850b9a23859f
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-27 11:10:13 +0900

    Add files via upload

--- Script/EnemyController_New.cs
+++ Script/EnemyController_New.cs
@@ -7,6 +7,11 @@ public class EnemyController_New : MonoBehaviour
     #region Variables
     protected StateMachine<EnemyController_New> stateMachine;
 
+    public LayerMask targetMask;
+    public Transform target;
+    public float viewRadius;
+    public float attackRange;
+
     #endregion Variables
 
     #region Unity Methods
@@ -26,4 +31,33 @@ public class EnemyController_New : MonoBehaviour
         stateMachine.Update(Time.deltaTime);
     }
     #endregion Unity Methods
+
+    #region Other Methods
+    public bool IsAvailableAttack
+    {
+        get
+        {
+            if (!target)
+            {
+                return false;
+            }
+
+            float distance = Vector3.Distance(transform.position, target.position);
+            return (distance <= attackRange);
+        }
+    }
+
+    public Transform SearchEnemy()
+    {
+        target = null;
+
+        Collider[] targetInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        if (targetInViewRadius.Length > 0)
+        {
+            target = targetInViewRadius[0].transform;
+        }
+
+        return target;
+    }
+    #endregion
 }
