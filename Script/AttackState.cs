2ba3ee81cb2919ad38b3002e89d301e73be41392
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-28 11:25:29 +0900

    Add files via upload

--- Script/AttackState.cs
+++ Script/AttackState.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackState : MonoBehaviour
+public class AttackState : State_New<EnemyController_New>
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AttackState_New : AttackState_New<EnemyController_New>
     {
-        
-    }
+        private Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private int hashAttack = Animator.StringToHash("Attack");
+
+        public override void OnInitialized()
+        {
+            animator = context.GetComponent<animator>();
+        }
+
+        public override void OnEnter()
+        {
+            if (context.IsAvailableAttack)
+            {
+                animator?.SetTrigger(hashAttack);
+            } 
+            else
+            {
+                statMachine.ChangeState<IdleState_New>();
+
+            }
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+        }
     }
 }
--- Script/MoveState.cs
+++ Script/MoveState.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MoveState : MonoBehaviour
+public class MoveState : Statae_New<EnemyController_New>
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private CharacterController controller;
+    private NavMeshAgent agent;
+
+    private int hashMove = Animator.StringToHash("Move");
+    private int hashMoveSpeed = Animator.StringToHash("MoveSpeed");
+
+    public override void OnInitialized()
     {
-        
+        animator = context.GetComponent<Animator>();
+        controller = context.GetComponent<CharacterController>();
+        agent = context.GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEnter()
     {
-        
+        agent?.SetDestination(context.target.position);
+        animator?.SetBool(hashMove, true);
     }
+
+    public override void Update(float deltaTime)
+    {
+        Transform enemy = context.SearchEnemy();
+        if (enemy)
+        {
+            agent.SetDestination(context.target.position);
+
+            if(agent.remainingDistance > agent.stoppingDistance)
+            {
+                controller.Move(agent.velocity * deltaTime);
+                animator.SetFloat(hashMoveSpeed, agent.velocity.magnitude / agent.speed, 1f, deltaTime);
+                return;
+
+            }
+        }
+
+        if (!enemy && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            stateMachine.ChangeState<IdleState_New>();
+        }
+    }
+
+    public override void OnExit()
+    {
+        animator?.SetBool(hashMove, false);
+        animator?.SetBool(hashMoveSpeed, 0f);
+        agent.ResetPath();
+    }
+
 }
