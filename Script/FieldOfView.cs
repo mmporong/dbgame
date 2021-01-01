26a2ce16bf9b8372e582849c9300824d0cbc6263
mmporong <74720422+mmporong@users.noreply.github.com>
2021-01-01 20:10:30 +0900

    Add files via upload

--- Script/FieldOfView.cs
+++ Script/FieldOfView.cs
@@ -11,6 +11,8 @@ public class FieldOfView : MonoBehaviour
 
     public LayerMask targetMask;
     public LayerMask obstacleMask;
+    
+    // Happy new year
     // Start is called before the first frame update
     void Start()
     {
