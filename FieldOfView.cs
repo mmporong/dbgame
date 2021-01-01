2848d01dbf7b2c2af699cb848219797586ef10a1
mmporong <74720422+mmporong@users.noreply.github.com>
2021-01-01 20:09:58 +0900

    Add files via upload

---
+++ FieldOfView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius = 5f;
+    [Range(0, 360)]
+
+    public float viewAngle = 90f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+    
+    // Happy new year
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
