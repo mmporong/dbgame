9078af5224ff795939bf487bbbd8ff9cc939a838
mmporong <74720422+mmporong@users.noreply.github.com>
2021-01-05 21:11:25 +0900

    Add files via upload

---
+++ FieldOfView.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius = 5f;
+    [Range(0, 360)]
+
+    public float viewAngle = 90f;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    private List<Transform> visibleTargets = new List<Transform>();
+    private Transform nearestTarget;
+    private float distanceToTarget = 0.0f;
+
+    public List<Transform> VisibleTargets => visibleTargets;
+
+    // Happy new year2
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindVisibleTargets();
+    }
+
+    void FindVisibleTargets()
+    {
+        distanceToTarget = 0.0f;
+        nearestTarget = null;
+        visibleTargets.Clear();
+
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                    if (nearestTarget == null || (distanceToTarget > dstToTarget))
+                    {
+                        nearestTarget = target;
+                        distanceToTarget = dstToTarget;
+                    }
+                }
+            }
+
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+
+        }
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
