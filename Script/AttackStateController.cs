2978136d9dff5b26c121a02c7f242885c68bc5eb
mmporong <74720422+mmporong@users.noreply.github.com>
2021-01-13 20:44:04 +0900

    Add files via upload

---
+++ Script/AttackStateController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackStateController : MonoBehaviour
+{
+    public delegate void OnEnterAttackState();
+    public delegate void OnExitAttackState();
+
+    public OnEnterAttackState enterAttackStateHandler;
+    public OnExitAttackState exitAttackStateHandler;
+
+    public bool IsInAttackState
+    {
+        get;
+        private set;
+    }
+
+    void Start()
+    {
+        enterAttackStateHandler = new OnEnterAttackState(EnterAttackState);
+        exitAttackStateHandler = new OnExitAttackState(ExitAttackState);
+    }
+    
+    #region Helper Methods
+    private void EnterAttackState()
+    {
+
+    }
+
+    private void ExitAttackState()
+    {
+
+    }
+
+    #endregion Helper Methods
+}
+
